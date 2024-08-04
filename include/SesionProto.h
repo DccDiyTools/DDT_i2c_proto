fb65476a4e4ff6cded9b9f186d1896d1a5a556da
Daniel Vilas Perulan <daniel.vilas@gmail.com>
2024-08-04 20:16:15 +0200

    Commands Sesion Proto

---
+++ include/SesionProto.h
@@ -0,0 +1,19 @@
+#ifndef SESION_PROTO
+#define SESION_PROTO
+
+#define CMD_Get_ID_R          0x01
+#define CMD_Get_Caps_R        0x02
+#define CMD_Get_Caps_proto_R  0x02
+#define CMD_Set_Addr_W        0x03
+#define CMD_Get_Addr_R        0x03
+#define CMD_Reset_W           0x00
+#define CMD_E_Stop_W          0x04
+#define CMD_Sotd_W            0x05
+#define CMD_Send_Int_W        0x06
+#define CMD_Ack_int_R         0x06
+#define CMD_read_event_R      0x07
+#define CMD_Init_W            0x0F
+#define CMD_get_status_R      0x0F 
+
+
+#endif
